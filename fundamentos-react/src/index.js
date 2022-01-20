fdd0b9d4a573abc313bc94416c243ed09df71962
DiogoCortezRibeiro <diogocortezr@gmail.com>
2022-01-20 08:29:08 -0300

    First commit

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Primeiro from './components/basicos/Primeiro.js';
+
+const el = document.getElementById('root');
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>
+    , el
+);
