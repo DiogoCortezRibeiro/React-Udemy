25ac1e9a3cf48b456273a7260b4c897cec80839f
Diogo cortez <diogocortez@gmail.com>
2022-03-03 16:42:38 -0300

    Criando aplicação em next.js

---
+++ fundamentos-next/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Inicio(){
+    return (
+        <div>
+            <h1>Fundamento de Next.js e React</h1>
+            <h3>Vamos explorar este framework</h3>
+        </div>
+    )
+}
---
+++ fundamentos-next/src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+export default function Jsx(){
+
+    const soma = 10 + 20;
+
+    return (
+        <div>
+            <h1>JSX é um conceito Central</h1>
+            <span>Resultado: {soma}</span>
+        </div>
+    )
+}
